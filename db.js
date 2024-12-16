e1e0452a053dac7b3986b4a94a74f7e61f70905b
Ana Grieser <anag@Anas-MacBook-Pro.local>
2024-12-16 14:31:22 -0700

    changed from client to pool

--- db.js
+++ db.js
@@ -1,35 +1,21 @@
 import pkg from "pg";
 import { getDatabaseUri } from "./config.js";
 
-const { Client } = pkg;
+const { Pool } = pkg;
 
 let db;
 
 if (process.env.NODE_ENV === "production") {
-	db = new Client({
+	db = new Pool({
 		connectionString: getDatabaseUri(),
 		ssl: {
 			rejectUnauthorized: false,
 		},
 	});
 } else {
-	db = new Client({
+	db = new Pool({
 		connectionString: getDatabaseUri(),
-		ssl: process.env.DATABASE_URL
-			? { rejectUnauthorized: false }
-			: undefined, // Only apply SSL if DATABASE_URL is defined
 	});
 }
-
-db.connect();
-
-db.connect((err) => {
-	if (err) {
-		console.error("Error connecting to the database:", err);
-		process.exit(1); // Exit the process if the database connection fails
-	} else {
-		console.log("Successfully connected to the database.");
-	}
-});
 
 export default db;
